710386e43bc36e8671c55463c5c8da98467457bf
Andrey Felipe Cubas <felipedreycubascardoso@gmail.com>
2025-03-17 20:53:22 -0300

    Nova forma de Organizar, Calculadora utilizando Funções e Modularização

--- Nivel-2/Calculadora/calculadore.py
+++ Nivel-2/Calculadora/calculadore.py
@@ -1,33 +1,50 @@
+def somar(num1, num2):
+    result = num1 + num2
+    print(f"Resultado: {result}")
+    return result
+
+def subtrair (num1, num2):
+    result = num1-num2
+    print(f"Resultado: {result}")
+    return result
+
+def multiplicar (num1,num2):
+    result = num1 * num2
+    print(f"Resultado: {result}")
+    return result
+
+def dividir (num1, num2):
+    result = num1/num2
+    print(f"Resultado: {result}")
+    return result
+
 def main():
-    while True:
-        print("Escolha a operação desejada:")
-        print("1. Adição")
-        print("2. Subtração")
-        print("3. Multiplicação")
-        print("4. Divisão")
-        print("5. Sair")
-        operacao =int(input("Digite a opcao desejada: "))
-        if operacao == 5:
-            print("Saindo da Calculadora")
-            break
-        num1 =float(input("Digite o primeiro numero: "))
-        num2 = float(input("Digite o segundo numero: "))
-        result = float()
-        if operacao == 1:
-            result = num1+num2
-            print("Resultado: ", result)
-        elif operacao == 2:
-            result = num1-num2
-            print("Resultado: ", result)
-        elif operacao == 3:
-            result = num1 * num2
-            print("Resultado: ", result)
-        elif operacao == 4:
-            if num2 != 0:
-                result = num1 / num2
-                print("Resultado: ", result)
-            else:
-                print("Nao da para dividir por 0")
-        else:
-            print("Operacao Invalida!")
+        
+        while True:
+            print("Escolha a operação desejada:")
+            print("1. Adição")
+            print("2. Subtração")
+            print("3. Multiplicação")
+            print("4. Divisão")
+            print("5. Sair")
+            operacao =int(input("Digite a opcao desejada: "))
+            if operacao == 5:
+                print("Saindo da Calculadora")
+                break
+            elif operacao >= 6 or operacao <= 0:
+                print("Opercao Invalida!")
+                continue
+            num1 =float(input("Digite o primeiro numero: "))
+            num2 = float(input("Digite o segundo numero: "))
+            if operacao == 1:
+                somar(num1, num2)
+            elif operacao == 2:
+                subtrair(num1, num2)
+            elif operacao == 3:
+                multiplicar(num1, num2)
+            elif operacao == 4:
+                if num2 != 0:
+                    dividir(num1, num2)
+                else:
+                    print("Nao da para dividir por 0")
 main()  
---
+++ Nível-3/Jogo-Advinhacao/advinhacao.py
@@ -0,0 +1,11 @@
+import random
+
+def gerar_numero ():
+    return random.randint(0,100)
+def palpites ():
+    num = int(input("tente advinhar o numero: "))
+    return num
+def 
+
+while True : 
+
